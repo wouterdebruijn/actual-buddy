3121621a82800fee4cb00fda46f41d8542e2bed7
Wouter de Bruijn <wouter@hedium.nl>
2026-02-28 12:26:28 +0100

    💄 Updated theme

--- src/integrations/mantine/theme.tsx
+++ src/integrations/mantine/theme.tsx
@@ -1,8 +1,19 @@
-import { createTheme } from "@mantine/core";
+import { createTheme, virtualColor } from "@mantine/core";
 
 const theme = createTheme({
 	defaultRadius: "md",
 	fontFamily: "Outfit, sans-serif",
+	primaryColor: "violet",
+	colors: {
+		primary: virtualColor({
+			name: "primary",
+			dark: "violet",
+			light: "violet",
+		}),
+	},
+	autoContrast: true,
+	black: "#444",
+	white: "#fff",
 });
 
 export default theme;
